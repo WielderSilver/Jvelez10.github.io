fff7003be1e9935d0e6507bb34f2632f68157479
Jvelez10 <74399879+Jvelez10@users.noreply.github.com>
2022-10-10 23:16:41 -0400

    Create fizzbuzz2.js

---
+++ Web115/scripts/fizzbuzz2.js
@@ -0,0 +1,36 @@
+"use strict";
+
+function greetChange() {
+	let firstName = document.getElementById('fName').value;
+	let midInitial = document.getElementById('mName').value;
+	let lastName = document.getElementById('lName').value;
+	let newGreet = `Greetings from Silverbeam Industries', ${firstName} ${midInitial} ${lastName}!`
+	document.getElementById('greeting').innerHTML = newGreet;
+	
+	let result = '';	
+	
+	const firstDiv = 3;
+	const secondDiv = 5;
+	
+	for (var i = 1; i <= 140; i++) {
+		result += i.toString();
+		if (!evenDiv(i, firstDiv) && !evenDiv(i, secondDiv)) {
+			result += "House";
+		}
+		else {
+			if (evenDiv(i, firstDiv)) {
+				result += "Residential Building";
+			}
+			if (evenDiv(i, secondDiv)) {
+				result += "=Skyscraper";
+			}
+		}
+		result += "<br>";
+	}
+	
+	document.getElementById('output').innerHTML = result;
+}
+
+function evenDiv(num1, num2) {
+	return (num1 % num2 == 0 ? true : false);
+}
