4028096b1fcba439f806eb637fbf55e550c4ed2a
Jvelez10 <74399879+Jvelez10@users.noreply.github.com>
2022-10-10 23:57:27 -0400

    Update fizzbuzz2.js

--- Web115/scripts/fizzbuzz2.js
+++ Web115/scripts/fizzbuzz2.js
@@ -7,11 +7,30 @@ function greetChange() {
 	let newGreet = `Welcome, ${firstName} ${midInitial} ${lastName}!`
 	document.getElementById('greeting').innerHTML = newGreet;
 	
-	let countMax = prompt(`How high do you want to count, ${firstName}?`)
-	let result = '';
-	for (var i = 1; i <= countMax; i++) {
-		i % 2 === 0 ? result += "Constructing... - the number is even<br>" : result += "Constructing... - the number is odd<br>";
+	let result = '';	
+	
+	const firstDiv = 3;
+	const secondDiv = 5;
+	
+	for (var i = 1; i <= 140; i++) {
+		result += i.toString();
+		if (!evenDiv(i, firstDiv) && !evenDiv(i, secondDiv)) {
+			result += " silk";
+		}
+		else {
+			if (evenDiv(i, firstDiv)) {
+				result += " spider";
+			}
+			if (evenDiv(i, secondDiv)) {
+				result += " web";
+			}
+		}
+		result += "<br>";
 	}
 	
 	document.getElementById('output').innerHTML = result;
+}
+
+function evenDiv(num1, num2) {
+	return (num1 % num2 == 0 ? true : false);
 }
