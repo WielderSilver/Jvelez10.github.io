1ecf5c869a7fa504b11f6ebe39688765b13437fa
Jvelez10 <74399879+Jvelez10@users.noreply.github.com>
2022-12-06 21:37:15 -0500

    Delete addHeader.php

--- web115/components/addHeader.php
+++
@@ -1,4 +0,0 @@
-<header>
-            <h1 style="text-align: center;">Silverbeam Industries</h1>
-            <h2 style="text-align: center;"> Upstanding infrastructure, Silverlining prices</h2>
-</header>
