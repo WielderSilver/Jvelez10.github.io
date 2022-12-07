977367c7c6e123833a8993b288764ad33cdde00c
Jvelez10 <74399879+Jvelez10@users.noreply.github.com>
2022-12-06 21:34:16 -0500

    Create addHeader.php
    testing

---
+++ web115/components/addHeader.php
@@ -0,0 +1,4 @@
+<header>
+            <h1 style="text-align: center;">Silverbeam Industries</h1>
+            <h2 style="text-align: center;"> Upstanding infrastructure, Silverlining prices</h2>
+</header>
